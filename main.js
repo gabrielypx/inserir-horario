3b731f235ba6f64a3eecac291c9dc08cec330800
Gabriely Paitax <gabrielypaitax@hotmail.com>
2022-01-06 11:19:43 -0300

    Update style

--- main.js
+++ main.js
@@ -10,22 +10,23 @@ function app() {
 
     if (hour <= new Date(`2000-01-01T12:00`) && hour >= new Date(`2000-01-01T00:00`)) {
       result.innerText = 'Bom Dia!';
-      result.style.color = "blue"
+      result.style.color = "#54a3c2"
     } else if (hour >= new Date(`2000-01-01T12:01`) && hour <= new Date(`2000-01-01T18:00`)) {
       result.innerText = 'Boa Tarde!';
-      result.style.color = "purple"
+      result.style.color = "#6c54c2"
     } else if (hour >= new Date(`2000-01-01T18:01`) && hour <= new Date(`2000-01-01T23:59`)) {
       result.innerText = 'Boa noite!';
-      result.style.color = "pink"
+      result.style.color = "#ec74dc"
     } else if (hourMinute[0] > 23) {
-      result.innerText = 'Você só pode informar um horario até 23:59.';  
-      result.style.color = "orange"
+      result.innerText = 'Você só pode informar um horário até 23:59.';  
+      result.style.color = "#e68659"
     } else if (hourMinute[1] > 59) {
       result.innerText = 'Os minutos vão só até 59.';  
-      result.style.color = "green"
+      result.style.color = "#59e665"
     } else {
-      result.innerText = 'Você precisa informar um horario no formato HH:mm';  
-      result.style.color = "red"
+      result.innerText = 'Você precisa informar um horário no formato HH:mm';  
+      result.style.color = "#df4040";
+      result.style.fontSize ="15px"
     }
 
     
